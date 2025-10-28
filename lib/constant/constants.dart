4a710d735f353682433e4bb1492ea9503e2abaa0
Buzurg Omid <ali.buzurg.omid@gmail.com>
2025-10-28 06:36:22 -0700

    Constants Created

---
+++ lib/constant/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color primaryColor = Colors.green;
+  static const Color blackColor = Colors.black;
+
+  static const String titleOne = "گیاهان را بهتر از قبل درک کن";
+  static const String descriptionOne =
+      "در مورد نگه داری گل و گیاهان میتوانی اطلاعات کسب کنی";
+  static const String titleTwo = "با گیاهان جدید آشنا شود";
+  static const String descriptionTwo =
+      "رز مشکی یا گل رز دوست داری ؟ اینجا میتوانی پیدایش کنی";
+  static const String titleThree = "با یک گل بهار نمیشود , گل بکار";
+  static const String descriptionThree =
+      "هر گلی نیاز داشته باشید در این برنامه پیدا میکنید";
+}
